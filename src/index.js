14f069dc7d88f371290b3db18e60305bb5f116f9
topazuser1 <56622884+topazuser1@users.noreply.github.com>
2025-02-08 16:14:53 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,8 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  "docker.funware.uk":  "https://registry-1.docker.io",
+  
 
   // staging
   ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
